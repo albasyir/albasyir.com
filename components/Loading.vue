2dd77c2d1cd31c8f80b74953620e8358d968c8b0
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2019-11-18 23:38:22 +0700

    Update Loading.vue

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,44 +1,67 @@
 <style scoped>
-  .loading {
-    width: 0;
+  .loading-enter-active, .loading-leave-active {
+    width: 100vw;
     height: 100vh;
     position: fixed;
     top: 0;
     left: 0;
-    background: blue;
+    background: #5e72e4;
+    transition: all 2000ms;
   }
+
+  .loading-enter {
+    width: 0;
+  }
+
+  .loading-leave-to {
+    left: 100vw;
+  }
+
+  .loadingTextContainer {
+    position: fixed;
+    top:0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    padding: 5vh 5vw;
+  }
+
+  .loadingTextContent {
+    font-size: 100pt; 
+    color: white;
+    font-weight: bolder;
+    -webkit-text-stroke-width: 2px;
+    -webkit-text-stroke-color: #5e72e4;
+    padding: 0 0 0 20px;
+  }  
 </style>
 
 <template>
-  <div ref='loadingLayer' class='loading'></div>
+  <transition name='loading'>
+    <div v-if='loading'>
+      <div class='loadingTextContainer'>
+        <div class='loadingTextContent'>
+          Please Wait...
+        </div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
 
   data: () => ({
-    speedOfTransition: 3000,
-    intervalFunc: null
+    loading: false
   }),
 
   methods: {
-
-    restartLoading() {
-
-      this.$refs.loadingLayer.style.transition = "none"
-      this.$refs.loadingLayer.style.left = 0
-      this.$refs.loadingLayer.style.width = 0
-      this.$refs.loadingLayer.style.transition = "all " + this.speedOfTransition + "ms"
-
-    },
-
     start() {
-      this.restartLoading()
-      this.$refs.loadingLayer.style.width = "100vw"
+      this.loading = true
     },
 
     finish() {
-      this.$refs.loadingLayer.style.left = "100vw"
+      this.loading = false
     }
   }
 }
