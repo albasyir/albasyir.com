562aa72b5ff720fe8ec48678dbdf927edbdcac7c
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2019-12-25 21:26:53 +0700

    create ListCard Components

---
+++ components/ListCard.vue
@@ -0,0 +1,41 @@
+<style scoped>
+    .listcard-key {
+        position: absolute;
+        top: -60px;
+        left: -38px;
+        font-weight: bold;
+        font-size: 70pt;
+        opacity: 0.5;
+    }
+</style>
+
+<template>
+    <div class='card shadow-lg m-3'>
+        <div class='listcard-key'>
+            {{ keylist }}
+        </div>
+        <div class='card-body py-3'>
+            <div class='mb-3'>
+                <span class='display-3'>
+                    {{ title }}
+                </span>
+            </div>
+            <div class='mb-3'>
+                {{ desc }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        keylist: {
+            type: [String],
+            required: true
+        },
+        title: String,
+        desc: String
+    }
+}
+</script>
--- components/Loading.vue
+++ components/Loading.vue
@@ -13,14 +13,19 @@
   }
 
   .slide-leave-active, .slide-enter-active {
-    width: 0;
-    height: 100vh;
     position: fixed;
-    transition: width 0.8s, height 0.8s, margin-left 1.5s, margin-top 1.5s;
+    top:0;
+    left:0;
     z-index: 999;
   }
 
   @media only screen and (min-width: 600px) {
+    .slide-leave-active, .slide-enter-active {
+      transition: width 0.8s, margin-left 1.5s;
+      width: 0;
+      height: 100vh;
+    }
+
     .slide-enter {
       margin-left: 0;
       width: 0;
@@ -33,13 +38,19 @@
   }
 
   @media only screen and (max-width: 600px) {
+    .slide-leave-active, .slide-enter-active {
+      transition: height 0.8s, margin-top 1.5s;
+      height: 0;
+      width: 100vw;
+    }
+
     .slide-enter {
       margin-top: 0;
       height: 0;
     }
 
     .slide-leave-to {
-      margin-top: 100vw;
+      margin-top: 100vh;
       height: 100%;
     }
   }
